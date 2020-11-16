0a23de1ca9e00a3ca7f83c1c2fab36d46b3ff804
joshua1909 <josheymail@hotmail.com>
2020-11-16 19:17:42 +1000

    New website version

--- js/common.js
+++ js/common.js
@@ -2,12 +2,19 @@ $(function() {
 
 	// Custom JS
 
+	// Preloader
+$(window).on('load', function() {
+	$('.preloader').delay(1000).fadeOut('slow');
+});
+
 	// Toggle mobile nav
 	$(".toggle-mnu").click(function() {
 		$(this).toggleClass("on");
 		$(".mobile-menu__nav").toggleClass("visible");
 		$("body").toggleClass("cover-bg");
 	});
+
+	$("ul.sf-menu").superfish();
 	
 
 	// Moble menu
@@ -39,21 +46,74 @@ $(function() {
 		owl.trigger("prev.owl.carousel");
 	});
 
-	// Count
-	$('.calc').each(function () {
-    $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 5000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-    });
-});
 
 // Equal heights for elements
 	$(".reach__item").equalHeights();
 
+// Partners carousel
+$(".partners").owlCarousel({
+	loop: true,
+	smartSpeed: 700,
+	dots: false,
+	nav: true,
+	navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+	responsiveClass: true,
+	responsive: {
+		0: {
+			items: 1
+		},
+		768: {
+			items: 2
+		},
+		992: {
+			items: 3
+		},
+		1200: {
+			items: 4
+		}
+	}
+});
+
+// Button to top
+$(window).scroll(function () {
+	if ($(this).scrollTop() > $(this).height()) {
+		$('.top').addClass('active');
+	}  else {
+		$('.top').removeClass('active');
+	}
+});
+
+// To top button
+$('.top').click(function () {
+	$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+})
+
+// Reviews
+$('.reviews').owlCarousel({
+	loop: true,
+	autoplay: true,
+	items: 1,
+	smartSpeed: 250,
+	autoplayHoverPause: true,
+	nav: false
+});
+
+//  Masonry 
+var $container = $(".masonry-container");
+
+	$container.imagesLoaded(function () {
+		$container.masonry({
+			itemSelector: ".item",
+			columnWidth: ".item"
+		});
+	});
+
+$('.grid-container').masonry({
+	itemSelector: '.grid-item',
+	columnWidth: 260,
+	gutter: 10
+})
 
 });
+
+
---
+++ libs/faqs/faqs.js
@@ -0,0 +1,15 @@
+var items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  var itemToggle = this.getAttribute('aria-expanded');
+  
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
